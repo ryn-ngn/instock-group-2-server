1f564b14a6b56ee163ec52c811012a3855e25de9
Elizabeth Sorescu <elizabeth.sorescu@gmail.com>
2024-03-18 08:16:31 -0700

    rename routes files and routes variable names in server.js file

--- server.js
+++ server.js
@@ -1,25 +1,25 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 const app = express();
 const PORT = process.env.PORT || 8080;
-const warehouses = require('./routes/warehouses')
-const inventory = require('./routes/inventory')
+const warehouseRoutes = require("./routes/warehouse-routes");
+const inventoryRoutes = require("./routes/inventory-routes");
 
 // Middleware
 app.use(cors()); // Enable CORS for all routes
 app.use(express.json());
 
 // Routes
-app.use('/warehouses', warehouses)
-app.use('/inventory', inventory)
+app.use("/warehouses", warehouseRoutes);
+app.use("/inventories", inventoryRoutes);
 
 // Error handling middleware
 app.use((err, req, res, next) => {
-    console.error(err.stack);
-    res.status(500).send('Something happened!');
+  console.error(err.stack);
+  res.status(500).send("Something happened!");
 });
 
 // Start server
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
