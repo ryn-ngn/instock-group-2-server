c9fe5bd5d3788968f7edc035bb27359f7f10e69f
Elizabeth Sorescu <elizabeth.sorescu@gmail.com>
2024-03-18 07:49:52 -0700

    add module exports router line 13

--- routes/inventory.js
+++ routes/inventory.js
@@ -1,11 +1,13 @@
-const express = require('express')
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
 
 //routes handlers
-router.route('/').get((req, res) => {
-    try {
-        res.status(200).json('this is inventory route')
-    } catch (error) {
-        res.status(400).send(`Couldn't retrieve inventory data: ${error}`);
-    }
-})
+router.route("/").get((req, res) => {
+  try {
+    res.status(200).json("this is inventory route");
+  } catch (error) {
+    res.status(400).send(`Couldn't retrieve inventory data: ${error}`);
+  }
+});
+
+module.exports = router;
--- routes/warehouses.js
+++ routes/warehouses.js
@@ -1,11 +1,13 @@
-const express = require('express')
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
 
 //routes handlers
-router.route('/').get((req, res) => {
-    try {
-        res.status(200).json('this is warehouses route')
-    } catch (error) {
-        res.status(400).send(`Couldn't retrieve warehouses data: ${error}`);
-    }
-})
+router.route("/").get((req, res) => {
+  try {
+    res.status(200).json("this is warehouses route");
+  } catch (error) {
+    res.status(400).send(`Couldn't retrieve warehouses data: ${error}`);
+  }
+});
+
+module.exports = router;
