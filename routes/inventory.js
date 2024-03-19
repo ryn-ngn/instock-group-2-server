2ef78521b71173aaedd8dda2773a5edbb2dbd4e8
Ryan Nguyen <ryan.ngn@outlook.com>
2024-03-19 19:37:49 -0400

    add dependencies
    add knexfile
    
    add code to knexfile
    
    change api endpoint of inventory route as per requirement
    
    add seeds file
    
    fix bug: change file path name to warehouses
    
    create seed data
    
    create controller
    
    create inventory controller connection
    
    add module exports in line 12
    
    install mysql dependencies
    
    fix bug: change to inventories instead of inventory
    
    intall npm cors for dependencies

--- routes/inventory.js
+++ routes/inventory.js
@@ -1,13 +1,7 @@
-const express = require('express')
-const router = express.Router()
+const router = require("express").Router();
+const inventoryController = require("../controllers/inventory-controller");
 
 //routes handlers
-router.route('/').get((req, res) => {
-    try {
-        res.status(200).json('this is inventory route')
-    } catch (error) {
-        res.status(400).send(`Couldn't retrieve inventory data: ${error}`);
-    }
-})
+router.route("/").get(inventoryController.index);
 
 module.exports = router;
