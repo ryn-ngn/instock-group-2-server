e6ee057e2ed8694743bfb63f4b4e1c16bcf4640d
Elizabeth Sorescu <elizabeth.sorescu@gmail.com>
2024-03-19 18:17:13 -0700

    add /:id endpoint to get one warehouse with specific id

--- routes/warehouses.js
+++ routes/warehouses.js
@@ -3,4 +3,7 @@ const warehouseController = require("../controllers/warehouse-controller");
 
 //routes handlers
 router.route("/").get(warehouseController.index);
+
+router.route("/:id").get(warehouseController.findOne);
+
 module.exports = router;
