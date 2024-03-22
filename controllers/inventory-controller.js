8b01c41212ed4fb672da36b23f4d1fa6e107b440
Elizabeth Sorescu <elizabeth.sorescu@gmail.com>
2024-03-21 20:44:08 -0700

    replace warehouse_id with warehouse_name data on the api endpoint fetch single warehouse

--- controllers/inventory-controller.js
+++ controllers/inventory-controller.js
@@ -3,7 +3,24 @@ const knex = require("knex")(require("../knexfile"));
 const index = async (_req, res) => {
   try {
     const data = await knex("inventories");
-    res.status(200).json(data);
+    const dataWarehouse = await knex("warehouses");
+
+    const inventoryData = data.map((item) => {
+      const container = {};
+      container.id = item.id;
+      container.warehouse_name = dataWarehouse.find(
+        (w) => w.id === item.warehouse_id
+      ).warehouse_name;
+      container.item_name = item.item_name;
+      container.description = item.description;
+      container.category = item.category;
+      container.status = item.status;
+      container.quantity = item.quantity;
+
+      return container;
+    });
+
+    res.status(200).json(inventoryData);
   } catch (error) {
     res.status(400).send(`Error retrieving inventories: ${error}`);
   }
