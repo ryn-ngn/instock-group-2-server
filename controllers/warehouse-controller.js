ae13e81f311e2b07d0dc5f45bab203756ea4844b
Elizabeth Sorescu <elizabeth.sorescu@gmail.com>
2024-03-23 11:05:57 -0700

    create delete warehouse function

--- controllers/warehouse-controller.js
+++ controllers/warehouse-controller.js
@@ -36,7 +36,7 @@ const getAllWarehouses = async (_req, res) => {
   }
 };
 
-//this is to find a single warehouse
+//This method will find a single warehouse with specific id
 const getWarehouseById = async (req, res) => {
   try {
     const warehousesFound = await knex("warehouses").where({
@@ -56,7 +56,29 @@ const getWarehouseById = async (req, res) => {
   }
 };
 
+// This method will delete a single warehouse with specific id
+const deleteWarehouseById = async (req, res) => {
+  try {
+    const rowsDeleted = await knex("warehouses")
+      .where({ id: req.params.id })
+      .delete();
+
+    if (rowsDeleted === 0) {
+      return res.status(404).json({
+        message: `Warehouse with ID ${req.params.id} is not found`,
+      });
+    }
+
+    res.sendStatus(204);
+  } catch (error) {
+    res.status(500).json({
+      message: `Unable to delete warehouse: ${error}`,
+    });
+  }
+};
+
 module.exports = {
   getAllWarehouses,
   getWarehouseById,
+  deleteWarehouseById,
 };
--- routes/warehouses.js
+++ routes/warehouses.js
@@ -4,6 +4,9 @@ const warehouseController = require("../controllers/warehouse-controller");
 //routes handlers
 router.route("/").get(warehouseController.getAllWarehouses);
 
-router.route("/:id").get(warehouseController.getWarehouseById);
+router
+  .route("/:id")
+  .get(warehouseController.getWarehouseById)
+  .delete(warehouseController.deleteWarehouseById);
 
 module.exports = router;
